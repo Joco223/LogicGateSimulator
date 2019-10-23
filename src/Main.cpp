46789a8ee11e515a946f421ad07f90816877aedd
Joco223 <jovanmihajlovic22@gmail.com>
2019-10-23 19:50:56 +0200

    File reorganization and performance improvement

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,7 +6,7 @@
 
 #include "Sandbox.h"
 #include "Camera.h"
-#include "GUI.h"
+#include "GUI_Controller.h"
 
 #include <iostream>
 #include <string>
@@ -27,28 +27,13 @@ int main() {
 	Simple2D::Text_context text_ctx("assets/Cascadia.ttf");
 
 	Sandbox sandbox(20, 20);
+	GUI_Controller gui_controller = GUI_Controller(ctx, width, height);
+	gui_controller.init_menu_bar(ctx, text_ctx);
 	Camera camera = Camera();
 	int rel_x = 0;
 	int rel_y = 0;
 	int curr_mouse_x = 0;
 	int curr_mouse_y = 0;
-
-	GUI::GUI gui(ctx, width, height);
-	GUI::menu_bar main_bar = GUI::menu_bar(0, height*0.97, width, height-height*0.97, {20, 20, 20, 255});
-	gui.add_menu_bar(&main_bar);
-	main_bar.add_button_sprite(ctx, "assets/wire_icon.png", "wire_button", {200, 200, 200, 255});
-	main_bar.add_button_text_sprite(text_ctx, ctx, "assets/chip_icon.png", "Chips", "chip_button", {200, 200, 200, 255});
-	main_bar.add_pop_up_window(ctx, 10, 550, 350, 20, {20, 20, 20, 255}, {15, 15, 15, 100}, "Chips", "chips_pop_up_window", "chip_button");
-	main_bar.add_pop_up_window_button(ctx, "assets/chip_icon.png",  "Test chip 1",  "test_chip1",  {20, 20, 20, 255}, {200, 200, 200, 255}, "chips_pop_up_window");
-	main_bar.add_pop_up_window_button(ctx, "assets/chip2_icon.png", "Test chip 2",  "test_chip2",  {20, 20, 20, 255}, {200, 200, 200, 255}, "chips_pop_up_window");
-	main_bar.add_pop_up_window_button(ctx, "assets/chip4_icon.png", "Test chip 3",  "test_chip3",  {20, 20, 20, 255}, {200, 200, 200, 255}, "chips_pop_up_window");
-	main_bar.add_pop_up_window_button(ctx, "assets/chip3_icon.png", "Test chip 4",  "test_chip4",  {20, 20, 20, 255}, {200, 200, 200, 255}, "chips_pop_up_window");
-	main_bar.add_pop_up_window_button(ctx, "assets/chip_icon.png",  "Test chip 5",  "test_chip5",  {20, 20, 20, 255}, {200, 200, 200, 255}, "chips_pop_up_window");
-	main_bar.add_pop_up_window_button(ctx, "assets/chip_icon.png",  "Test chip 6",  "test_chip6",  {20, 20, 20, 255}, {200, 200, 200, 255}, "chips_pop_up_window");
-	main_bar.add_pop_up_window_button(ctx, "assets/chip_icon.png",  "Test chip 7",  "test_chip7",  {20, 20, 20, 255}, {200, 200, 200, 255}, "chips_pop_up_window");
-	main_bar.add_pop_up_window_button(ctx, "assets/chip4_icon.png", "Test chip 8",  "test_chip8",  {20, 20, 20, 255}, {200, 200, 200, 255}, "chips_pop_up_window");
-	main_bar.add_pop_up_window_button(ctx, "assets/chip3_icon.png", "Test chip 9",  "test_chip9",  {20, 20, 20, 255}, {200, 200, 200, 255}, "chips_pop_up_window");
-	main_bar.add_pop_up_window_button(ctx, "assets/chip4_icon.png", "Test chip 10", "test_chip10", {20, 20, 20, 255}, {200, 200, 200, 255}, "chips_pop_up_window");
 
 	while(!ctx.check_exit()) {
 		ctx.clear();
@@ -67,34 +52,21 @@ int main() {
 		std::optional<Simple2D::mouse_button_e> mouse_button_event = ctx.check_mouse_button();
 		if(mouse_button_event) {
 			camera.update_position(*mouse_button_event);
-			gui.handle_mouse_click(*mouse_button_event);
+			gui_controller.handle_mouse_click(*mouse_button_event);
 		}
 
 		std::optional<Simple2D::mouse_wheel_e> mouse_wheel_event = ctx.check_mouse_wheel();
 		if(mouse_wheel_event) {
-			main_bar.check_pop_up_tile_scroll("chips_pop_up_window", *mouse_wheel_event, curr_mouse_x, curr_mouse_y);
+			gui_controller.handle_mouse_scroll(*mouse_wheel_event, curr_mouse_x, curr_mouse_y);
 		}
+
+		gui_controller.handle_events();
 
 		camera.move_camera(rel_x, rel_y);
 
 		sandbox.draw_grid(camera.get_pos_x(), camera.get_pos_y(), ctx);
 
-		gui.draw_gui(text_ctx, ctx);
-
-		GUI::event gui_event = gui.peek_event_stack();
-		while(gui_event.event_type != -1) {
-			if(gui_event.source == "wire_button") {
-				if(gui_event.event_type == Simple2D::MOUSE_DOWN && gui_event.event_desc_bool) {
-					main_bar.set_button_state("wire_button", !main_bar.get_button_state("wire_button"));
-				}
-			}else if(gui_event.source == "chip_button") {
-				if(gui_event.event_type == Simple2D::MOUSE_DOWN && gui_event.event_desc_bool) {
-					main_bar.set_button_state("chip_button", !main_bar.get_button_state("chip_button"));
-					main_bar.set_pop_up_window_direction("chips_pop_up_window", !main_bar.get_pop_up_window_direction("chips_pop_up_window"));
-				}
-			}
-			gui_event = gui.peek_event_stack();
-		}
+		gui_controller.draw_gui(ctx, text_ctx);
 
 		ctx.draw();
 	}
